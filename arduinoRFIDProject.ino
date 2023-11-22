cfa5a9452ce34038eb64e40f63a4286cb71eded6
Sushi <117356257+Supr3mSushi@users.noreply.github.com>
2023-11-23 00:09:02 +0100

    Update and rename sketch_nov22a.ino to arduinoRFIDProject.ino

--- arduinoRFIDProject.ino
+++ arduinoRFIDProject.ino
@@ -2,12 +2,10 @@
 #include <MFRC522.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
 
 #define RST_PIN 2   // Broche de réinitialisation (RST) du lecteur RFID
 #define SS_PIN  6    // Broche SDA du lecteur RFID (pour ESP32)
-
-// const char apiURL = ""; // API 
-// const charbadgeInfoFile = "badge_info.json"; // Nom du fichier JSON contenant les informations des badges
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Crée un objet MFRC522
 
@@ -17,11 +15,11 @@ byte authorizedUID[][4] = {
   {0x14, 0x99, 0xE3, 0x62},
 };
 
-const char *ssid = "GAMINGCAMPUS_ELEVES";
-const char *password = "Gam1nG69007";
+const char *ssid = "NUMERICABLE-82B5";
+const char *password = "KHC2EA722X";
 
-const char* raspberryPiIP = "192.168.1.2";
-const int raspberryPiPort = 8080;
+const char* apiIP = "192.168.1.2":
+const int apiPort = 8080; // Le port n'est peut etre pas le bon
 
 // Fonction pour se connecter au WiFi
 void connectToWiFi() {
@@ -36,17 +34,29 @@ void connectToWiFi() {
   Serial.println("\nConnected to WiFi");
 }
 
-void sendDataToRaspberryPi(const char* data) {
-  Serial.print("Sending data to Raspberry Pi: ");
+void sendDataToAPI(const char* data) {
+  Serial.print("Sending data to API: ");
   Serial.println(data);
 
   WiFiClient client;
-  if (client.connect(raspberryPiIP, raspberryPiPort)) {
+
+  if (client.connect(apiIP, apiPort)) {
+    client.print("POST /votre-route HTTP/1.1\r\n"); // Saisir l'URL de l'API
+    client.print("Host: ");
+    client.print(apiIP);
+    client.print("\r\n");
+    client.print("Content-Type: application/json\r\n");
+    client.print("Content-Length: ");
+    client.print(strlen(data));
+    client.print("\r\n\r\n");
     client.print(data);
-    client.stop();
+
+    Serial.println("Data sent to API");
   } else {
     Serial.println("Failed to connect to Raspberry Pi");
   }
+
+  client.stop();
 }
 
 void cleanupAuthorizedUID() {
@@ -75,6 +85,7 @@ void loop() {
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
   digitalWrite(7, LOW);
+
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     Serial.print("Carte détectée avec UID : ");
     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
@@ -82,12 +93,12 @@ void loop() {
 
     if (compareUID(authorizedUID, mfrc522.uid.uidByte, mfrc522.uid.size)) {
       Serial.println("Accès autorisé !");
-      sendDataToRaspberryPi("Access Granted");
+      sendDataToAPI("Access Granted");
       digitalWrite(3, HIGH);
       delay(1000);
     } else {
       Serial.println("Accès refusé !");
-      sendDataToRaspberryPi("Access Denied");
+      sendDataToAPi("Access Denied");
       digitalWrite(4, HIGH);
       delay(1000);
     }
