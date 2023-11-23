ea5f78457f974a2480bfd423b10276cd4ba98e11
Sushi <117356257+Supr3mSushi@users.noreply.github.com>
2023-11-23 05:13:02 +0100

    Update arduinoRFIDProject.ino

--- arduinoRFIDProject.ino
+++ arduinoRFIDProject.ino
@@ -18,8 +18,8 @@ byte authorizedUID[][4] = {
 const char *ssid = "NUMERICABLE-82B5";
 const char *password = "KHC2EA722X";
 
-const char* apiIP = "192.168.1.2":
-const int apiPort = 8080; // Le port n'est peut etre pas le bon
+const char* apiIP = "192.168.1.2";
+const int apiPort = 3000;
 
 // Fonction pour se connecter au WiFi
 void connectToWiFi() {
@@ -34,14 +34,82 @@ void connectToWiFi() {
   Serial.println("\nConnected to WiFi");
 }
 
-void sendDataToAPI(const char* data) {
-  Serial.print("Sending data to API: ");
-  Serial.println(data);
+// Fonction pour envoyer une requête POST
+void sendPostRequest(const char* url, const char* data, int port) {
+  WiFiClient client;
 
+  if (client.connect(apiIP, port)) {
+    client.print("POST ");
+    client.print(url);
+    client.print(" HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(apiIP);
+    client.print(":");
+    client.print(port);
+    client.print("\r\n");
+    client.print("Content-Type: application/json\r\n");
+    client.print("Content-Length: ");
+    client.print(strlen(data));
+    client.print("\r\n\r\n");
+    client.print(data);
+
+    Serial.println("POST Request sent to API");
+  } else {
+    Serial.println("Failed to connect to API");
+  }
+
+  client.stop();
+}
+
+
+// Fonction pour envoyer une requête GET
+void sendGetRequest(const char* url) {
   WiFiClient client;
 
   if (client.connect(apiIP, apiPort)) {
-    client.print("POST /votre-route HTTP/1.1\r\n"); // Saisir l'URL de l'API
+    client.print("GET ");
+    client.print(url);
+    client.print(" HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(apiIP);
+    client.print("\r\n\r\n");
+
+    Serial.println("GET Request sent to API");
+  } else {
+    Serial.println("Failed to connect to API");
+  }
+
+  client.stop();
+}
+
+// Fonction pour envoyer une requête DELETE
+void sendDeleteRequest(const char* url) {
+  WiFiClient client;
+
+  if (client.connect(apiIP, apiPort)) {
+    client.print("DELETE ");
+    client.print(url);
+    client.print(" HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(apiIP);
+    client.print("\r\n\r\n");
+
+    Serial.println("DELETE Request sent to API");
+  } else {
+    Serial.println("Failed to connect to API");
+  }
+
+  client.stop();
+}
+
+// Fonction pour envoyer une requête PUT
+void sendPutRequest(const char* url, const char* data) {
+  WiFiClient client;
+
+  if (client.connect(apiIP, apiPort)) {
+    client.print("PUT ");
+    client.print(url);
+    client.print(" HTTP/1.1\r\n");
     client.print("Host: ");
     client.print(apiIP);
     client.print("\r\n");
@@ -51,18 +119,40 @@ void sendDataToAPI(const char* data) {
     client.print("\r\n\r\n");
     client.print(data);
 
-    Serial.println("Data sent to API");
+    Serial.println("PUT Request sent to API");
   } else {
-    Serial.println("Failed to connect to Raspberry Pi");
+    Serial.println("Failed to connect to API");
   }
 
   client.stop();
 }
 
-void cleanupAuthorizedUID() {
-  // Ici, vous pourriez effectuer des opérations de nettoyage si nécessaire.
-  // Par exemple, effacer les données de la carte autorisée après un certain temps.
+// Fonction pour envoyer une requête PATCH
+void sendPatchRequest(const char* url, const char* data) {
+  WiFiClient client;
+
+  if (client.connect(apiIP, apiPort)) {
+    client.print("PATCH ");
+    client.print(url);
+    client.print(" HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(apiIP);
+    client.print("\r\n");
+    client.print("Content-Type: application/json\r\n");
+    client.print("Content-Length: ");
+    client.print(strlen(data));
+    client.print("\r\n\r\n");
+    client.print(data);
+
+    Serial.println("PATCH Request sent to API");
+  } else {
+    Serial.println("Failed to connect to API");
+  }
+
+  client.stop();
 }
+
+// ... Autres fonctions nécessaires ...
 
 void setup() {
   Serial.begin(115200);  // Initialise la communication série
@@ -93,26 +183,23 @@ void loop() {
 
     if (compareUID(authorizedUID, mfrc522.uid.uidByte, mfrc522.uid.size)) {
       Serial.println("Accès autorisé !");
-      sendDataToAPI("Access Granted");
+
+      // Exemple d'utilisation des fonctions avec le port spécifié
+      sendPostRequest("/api/badges", "{\"badge_id\":\"123\", \"name\":\"John Doe\"}", 3000);
+      sendGetRequest("/api/badges", 3000);
+      sendPutRequest("/api/badges/{id}", "{\"badge_id\":\"123\", \"name\":\"John Smith\"}", 3000);
+      sendDeleteRequest("/api/badges/{id}", 3000);
+      sendPatchRequest("/api/badges/{badge_id}/scan", "{\"scanned\":true}", 3000);
+
       digitalWrite(3, HIGH);
       delay(1000);
     } else {
       Serial.println("Accès refusé !");
-      sendDataToAPi("Access Denied");
       digitalWrite(4, HIGH);
       delay(1000);
     }
 
     mfrc522.PICC_HaltA();
   }
-  cleanupAuthorizedUID();
-}
-
-bool compareUID(byte storedUID[][4], byte* detectedUID, byte size) {
-  for (byte i = 0; i < 2; i++) {
-    if (memcmp(storedUID[i], detectedUID, size) == 0) {
-      return true;
-    }
-  }
-  return false;
+  // cleanupAuthorizedUID();
 }
